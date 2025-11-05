0c64d7e54676fb8d4b857a6deb1008d4259be4e1
Great Lucky <lgreat098@gmail.com>
2025-11-05 21:07:22 +0100

    made some modifications

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -8,7 +8,6 @@ export const ProductDisplay = () => {
   return (
     <div className="flex flex-col gap-12 ">
       <ProductTitle />
-
       <ul className="flex flex-col md:grid grid-cols-3 gap-x-16 gap-y-24 justify-center gap-20">
         {products.map((product) => (
           <ProductList key={product.name} product={product} />
